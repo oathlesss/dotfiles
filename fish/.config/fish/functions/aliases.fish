ef35cb01984510ce9b18a028218f278d8d83cbd0
Ruben Hesselink <rubenhesselink@outlook.com>
2025-04-15 19:58:53 +0200

    wooo

--- fish/.config/fish/functions/aliases.fish
+++ fish/.config/fish/functions/aliases.fish
@@ -1,3 +1,3 @@
 alias lnvim "NVIM_APPNAME=lazynvim nvim"
-alias knvim "NVIM_APPNAME=kickstart.nvim nvim"
+alias knvim "NVIM_APPNAME=knvim nvim"
 alias nvim_conf "nvim ~/Developer/dev-conf/nvim/.config/nvim"
