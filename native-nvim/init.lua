113424ebe6b51918cfc8618953189e6c01a89744
Ruben Hesselink <ruben@fourdigits.nl>
2025-07-15 13:52:30 +0200

    Take some inspiration from the dotfiles of the author of vague.nvim.

--- native-nvim/init.lua
+++
@@ -1,9 +0,0 @@
-if vim.fn.has("nvim-0.11") == 0 then
-	vim.notify("NativeVim only supports Neovim 0.11+", vim.log.levels.ERROR)
-	return
-end
-
-require("core.mini-deps")
-require("core.mini")
-require("core.options")
-require("core.lsp")
